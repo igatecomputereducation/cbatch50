c5836d16c67f2f8fada86da01aab871d7839d51f
igate solutions <igatecomputereducation@gmail.com>
2023-06-03 11:40:17 +0530

    v1

--- demo.c
+++ demo.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	
 	int num;
-	
-	num=50;
-	
-	printf("%i is called %i of %i",num,num,nun);
-	
-	
+	printf("Enter any integer:");
+	scanf("%i",&num);
+	if(num>0)
+		printf("positive\n");
+	if(num<0)
+		printf("negative\n");
+	if(num==0)
+		printf("zero");
 	return 0;
 }
 
