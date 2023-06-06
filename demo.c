3fd3f05aaefc6e1864c508bf3363a675ae3f926e
igate solutions <igatecomputereducation@gmail.com>
2023-06-06 09:54:01 +0530

    v1

--- demo.c
+++ demo.c
@@ -1,15 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int num;
-	printf("Enter any integer:");
-	scanf("%i",&num);
-	if(num>0)
-		printf("positive\n");
-	if(num<0)
-		printf("negative\n");
-	if(num==0)
-		printf("zero");
+	
 	return 0;
 }
 
---
+++ exp26.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	if(10<20)
+	{
+		printf("hello\n");
+		printf("world\n");
+	}
+	if(40>=20)
+		printf("kavali\n");
+	if(10!=10)
+		printf("igate\n");2
+	return 0;
+}
