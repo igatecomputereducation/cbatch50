05f6868a0372300a6d112f830baac1f94952d576
igate <igatecomputereducation@gmail.com>
2023-06-30 09:59:51 +0530

    v1

---
+++ lab120.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int i,base,exp,res;
+	printf("Base:");
+	scanf("%i",&base);
+	printf("Exponent:");
+	scanf("%i",&exp);
+	for(res=1,i=1;i<=exp;i++)
+		res=res*base;
+	printf("Result %i",res);
+	return 0;
+}
