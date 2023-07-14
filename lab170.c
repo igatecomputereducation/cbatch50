64acc1f55fcd52fe59847807199d5ab6f6fbfdfe
igatecomputereducation <igatecomputereducation@gmail.com>
2023-07-14 17:12:04 +0530

    v1

---
+++ lab170.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+	int a[50],i,n,pos;
+	
+	printf("How many elements:");
+	scanf("%i",&n);
+	
+	printf("Enter elements..\n");
+	for(i=0;i<n;i++)
+		scanf("%i",&a[i]);
+		
+	printf("Enter position to insert:");
+	scanf("%i",&pos);
+	
+	if(pos<1 || pos>n)
+		printf("Invalid position");
+	else
+	{
+		pos--;
+		
+		for(i=n-1;i>=pos;i--)
+			a[i+1]=a[i];
+			
+		printf("Enter the element:");
+		scanf("%i",&a[pos]);
+		
+		n++;
+		
+		printf("Result array..\n");
+		for(i=0;i<n;i++)
+			printf("%5i",a[i]);
+	}
+	
+	return 0;
+}
+
+
+
+
+
